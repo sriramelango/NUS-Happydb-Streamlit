d4c4c329e3464811053c41b51045cb7469f9dad2
Sriram Elango <sriram@fireflies.ai>
2022-01-11 20:35:27 -0500

    Refactor and Demographic Analysis

--- app.py
+++ app.py
@@ -9,25 +9,6 @@ def normalizeData(trainingData):
     trainingData['age'] = pd.to_numeric(trainingData['age'], errors='coerce')
     return trainingData
 
-# Obtain and Process Data
-trainingDF = pd.read_csv("./data/labeledDataTrain.csv")
-testDF = pd.read_csv("./data/labeledDataTest.csv",sep=",",encoding = 'cp1252')
-trainingDF = normalizeData(trainingDF)
-testDF = normalizeData(testDF)
-
-st.set_page_config(page_title="CL-Aff Shared Task - In Pursuit of Happiness", layout="wide")
-
-st.title("CL-Aff Shared Task - In Pursuit of Happiness")
-st.markdown("""
-* Corpus and annotations for the CL-Aff Shared Task - In Pursuit of Happiness - from the University of Pennsylvania
-* A part of the AffCon Workshop @ AAAI 2019 for Modeling Affect-in-Action
-* Check out the Workshop and Shared Task website: https://sites.google.com/view/affcon2019/home
-""")
-
-option = st.selectbox("What dataset would you like to explore?",("Training","Test"))
-st.write('You selected:', option)
-
-
 def dataHistogramProcess(data,xlabel,ylabel):
     dataFiltered = []
     dataUnique = data.unique()
@@ -86,8 +67,6 @@ def emotionHistogram(data):
     c = alt.Chart(emotionalFrequency).mark_bar().encode(x='Labels',y='Frequency').interactive()
     st.altair_chart(c, use_container_width=True)
 
-
-
 def demographicViewer(country, data):
     countryDataset = data[data["country"] == country]
     ageHistogram(countryDataset["age"])
@@ -96,59 +75,55 @@ def demographicViewer(country, data):
     genderHistorgram(countryDataset["gender"])
     parentHoodHistogram(countryDataset["parenthood"])
     emotionHistogram(countryDataset)
+   
+# Obtain and Process Data
+trainingDF = normalizeData(pd.read_csv("./data/labeledDataTrain.csv"))
+testDF = normalizeData(pd.read_csv("./data/labeledDataTest.csv",sep=",",encoding = 'cp1252'))
 
-    
+# Set UI and Interface
+st.set_page_config(page_title="CL-Aff Shared Task - In Pursuit of Happiness", layout="wide")
+
+st.title("CL-Aff Shared Task - In Pursuit of Happiness")
+
+st.markdown("""
+* Corpus and annotations for the CL-Aff Shared Task - In Pursuit of Happiness - from the University of Pennsylvania
+* A part of the AffCon Workshop @ AAAI 2019 for Modeling Affect-in-Action
+* Check out the Workshop and Shared Task website: https://sites.google.com/view/affcon2019/home
+""")
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#User Interaction
+option = st.selectbox("What dataset would you like to explore?",("Training","Test"))
+st.write('You selected:', option)
 
 if option == "Training":
     st.write(trainingDF) 
     st.title('Demographics')
-    option = st.selectbox("What nation would you like to explore?",("Training","Test"))
-
-
-
-
-
-
-
-
-    ageHistogram(trainingDF["age"])
-    nationHistogram(trainingDF["country"])
-    marriageHistogram(trainingDF["married"])
-    durationHistogram(trainingDF["duration"])
-    genderHistorgram(trainingDF["gender"])
-    parentHoodHistogram(trainingDF["parenthood"])
-    emotionHistogram(trainingDF)
-    st.title('Demographics')
-
+    selectionOptions = pd.DataFrame(trainingDF["country"].unique()).dropna().append(["ALL"])
+    nation = st.selectbox("Where would you like to explore?", selectionOptions)
+    if nation == "ALL":
+        ageHistogram(trainingDF["age"])
+        nationHistogram(trainingDF["country"])
+        marriageHistogram(trainingDF["married"])
+        durationHistogram(trainingDF["duration"])
+        genderHistorgram(trainingDF["gender"])
+        parentHoodHistogram(trainingDF["parenthood"])
+        emotionHistogram(trainingDF)
+    else:
+        demographicViewer(nation, trainingDF)
 
 if option == "Test":
-    st.write(testDF)
+    st.write(testDF) 
     st.title('Demographics')
-
-
-
-
-    
-    ageHistogram(testDF["age"])
-    nationHistogram(testDF["country"])
-    marriageHistogram(testDF["married"])
-    durationHistogram(testDF["duration"])
-    genderHistorgram(testDF["gender"])
-    parentHoodHistogram(testDF["parenthood"])
-    emotionHistogram(testDF)
+    selectionOptions = pd.DataFrame(testDF["country"].unique()).dropna().append(["ALL"])
+    nation = st.selectbox("Where would you like to explore?", selectionOptions)
+    if nation == "ALL":
+        ageHistogram(testDF["age"])
+        nationHistogram(testDF["country"])
+        marriageHistogram(testDF["married"])
+        durationHistogram(testDF["duration"])
+        genderHistorgram(testDF["gender"])
+        parentHoodHistogram(testDF["parenthood"])
+        emotionHistogram(testDF)
+    else:
+        demographicViewer(nation, testDF)
